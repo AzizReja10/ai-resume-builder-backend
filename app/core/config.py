fb4c7f109df8ede592ebe7603d716e084618be8d
AzizReja10 <rejaaziz686@gmail.com>
2026-08-09 00:39:52 +0530

    changes

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,15 @@
 from pydantic_settings import BaseSettings
+
+
 class Settings(BaseSettings):
-    database_url:str
-    secret_key:str
-    algorithm:str
-    access_token_expire_minutes:int=60
-    groq_api_key:str
+    database_url: str
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int = 60
+    groq_api_key: str
+
     class Config:
-        env_file=".env"
-settings=Settings()
+        env_file = ".env"
+
+
+settings = Settings()
