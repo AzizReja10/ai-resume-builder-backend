cb3caaba7f84025e18aa64bd989da54c53f09fc2
AzizReja10 <rejaaziz686@gmail.com>
2026-08-09 00:59:41 +0530

    changes3

--- app/core/database.py
+++ app/core/database.py
@@ -1,11 +1,19 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    create_async_engine,
+    async_sessionmaker,
+)
 from sqlalchemy.orm import DeclarativeBase
+
 from app.core.config import settings
 
 
 engine = create_async_engine(
     settings.database_url,
     echo=True,
+    connect_args={
+        "ssl": "require"
+    }
 )
 
 AsyncSessionLocal = async_sessionmaker(
