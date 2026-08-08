e587d5a652a8d51d508c73e0e60020f5ce1e089b
AzizReja10 <rejaaziz686@gmail.com>
2026-08-09 00:46:20 +0530

    changes2

--- app/core/database.py
+++ app/core/database.py
@@ -6,7 +6,6 @@ from app.core.config import settings
 engine = create_async_engine(
     settings.database_url,
     echo=True,
-    connect_args={"ssl": "require"}
 )
 
 AsyncSessionLocal = async_sessionmaker(
