dc69ed1949506a0668581b8a6fa1696bfa043eac
AzizReja10 <rejaaziz686@gmail.com>
2026-08-09 02:08:57 +0530

    Fix CORS configuration

--- app/main.py
+++ app/main.py
@@ -1,26 +1,42 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.core.database import engine, Base
 from app.auth.router import router as auth_router
 from app.auth import models as auth_models
 from app.resumes import models as resume_models
 from app.resumes.router import router as resumes_router
-from fastapi.middleware.cors import CORSMiddleware
+
+
 app = FastAPI(title="AI Resume Builder")
+
+
+# CORS
+origins = [
+    "http://localhost:5173",
+    "https://ai-resume-builder-frontend-sigma.vercel.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Routers
 app.include_router(resumes_router)
 app.include_router(auth_router)
+
 
 @app.on_event("startup")
 async def startup():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
+
 @app.get("/health")
 async def health():
-    return {"status": "ok" }
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://ai-resume-builder-frontend-ebxf8vbgd-aziz-rejas-projects.vercel.app","https://ai-resume-builder-frontend-sigma.vercel.app/"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+    return {"status": "ok"}
